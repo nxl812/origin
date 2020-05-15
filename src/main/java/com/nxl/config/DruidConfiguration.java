ed9b0c2b7d2d1842e31dac61a7389939a2080c9d
naixianlei <naixianlei@zqykj.com>
2020-05-15 11:33:23 +0800

    druid监控结合shiro

--- src/main/java/com/nxl/config/DruidConfiguration.java
+++ src/main/java/com/nxl/config/DruidConfiguration.java
@@ -1,19 +1,120 @@
 package com.nxl.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+@Getter
+@Setter
+@NoArgsConstructor
 public class DruidConfiguration {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.filters}")
+    private String filters;
+    @Value("${spring.datasource.connectionProperties}")
+    private String connectionProperties;
+    @Value("${spring.datasource.useGlobalDataSourceStat}")
+    private boolean useGlobalDataSourceStat;
 
-    @ConfigurationProperties(prefix = "spring.datasource")
+
+
+    //注册 Servlet 组件
     @Bean
-    public DataSource dataSource(){
-        return new DruidDataSource();
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1"); //白名单IP
+//        servletRegistrationBean.addInitParameter("deny", "127.0.0.1"); //黑名单IP，如和白名单冲突，黑优先
+        servletRegistrationBean.addInitParameter("resetEnable", "false"); //白名单IP
+        servletRegistrationBean.addInitParameter("loginUsername", "naixianlei");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        return servletRegistrationBean;
+    }
+
+    // 注册 Filter 组件
+    @Bean
+    public FilterRegistrationBean statFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //验证所有请求
+        filterRegistrationBean.addUrlPatterns("/*");
+        //对 *.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/* 不进行验证
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+    //配置数据源
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        //configuration
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setPoolPreparedStatements(poolPreparedStatements);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        dataSource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);
+        try {
+            dataSource.setFilters(filters);
+        } catch (SQLException e) {
+            System.err.println("druid configuration initialization filter: "+ e);
+        }
+        dataSource.setConnectionProperties(connectionProperties);
+        return dataSource;
     }
 
 }
--- src/main/java/com/nxl/controller/JumpController.java
+++ src/main/java/com/nxl/controller/JumpController.java
@@ -3,12 +3,19 @@ package com.nxl.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class JumpController {
 
     @GetMapping("/gotoSuccess")
     public String gotoSuccess(){
         return "success";
+    }
+
+    @GetMapping("/druidHtml")
+    public String druidHtml(){
+        return "redirect:/druid/login.html";
     }
 
 }
