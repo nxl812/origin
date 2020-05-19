e17c83b479cf7c81026a8161fa0a6c3387298e8e
naixianlei <naixianlei@zqykj.com>
2020-05-19 21:11:28 +0800

    记录shiro结合cas实现sso

--- src/main/java/com/nxl/shiro/MyCasRealm.java
+++ src/main/java/com/nxl/shiro/MyCasRealm.java
@@ -4,6 +4,9 @@ import org.apache.shiro.cas.CasRealm;
 
 public class MyCasRealm extends CasRealm {
     //cas需要单独服务，不想弄了
+    //妈蛋，干！
     //https://www.cnblogs.com/funyoung/p/9242220.html
     //https://www.cnblogs.com/funyoung/p/9234947.html
+//    https://github.com/apereo/cas-overlay-template/tree/5.3
+    //https://www.cnblogs.com/flying607/p/7605158.html
 }
